d58814bf8e861a687a2d068e891f8478a61751a7
NOur <44316424+nourabosen@users.noreply.github.com>
2025-09-14 00:03:53 +0300

    Add files via upload

--- src/templates/IndexTemplate/IndexTemplate.tsx
+++ src/templates/IndexTemplate/IndexTemplate.tsx
@@ -2,11 +2,9 @@ import React from "react";
 
 import { graphql } from "gatsby";
 
-import { Feed } from "@/components/Feed";
+import { Home } from "@/components/Home";
 import { Layout } from "@/components/Layout";
 import { Meta } from "@/components/Meta";
-import { Page } from "@/components/Page";
-import { Pagination } from "@/components/Pagination";
 import { Sidebar } from "@/components/Sidebar";
 import { useSiteMetadata } from "@/hooks";
 import { AllMarkdownRemark, PageContext } from "@/types";
@@ -19,23 +17,12 @@ interface Props {
 }
 
 const IndexTemplate: React.FC<Props> = ({ data, pageContext }: Props) => {
-  const { pagination } = pageContext;
-  const { hasNextPage, hasPrevPage, prevPagePath, nextPagePath } = pagination;
-
   const { edges } = data.allMarkdownRemark;
 
   return (
     <Layout>
       <Sidebar isIndex />
-      <Page>
-        <Feed edges={edges} />
-        <Pagination
-          prevPagePath={prevPagePath}
-          nextPagePath={nextPagePath}
-          hasPrevPage={hasPrevPage}
-          hasNextPage={hasNextPage}
-        />
-      </Page>
+      <Home edges={edges} pageContext={pageContext} />
     </Layout>
   );
 };
