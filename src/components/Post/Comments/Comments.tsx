84850b2cb0ef73dadfc66ed54d1d55963bdfbbc9
nourabosen <nraabosen@gmail.com>
2025-10-30 22:59:55 +0200

    fix: address user feedback

--- src/components/Post/Comments/Comments.tsx
+++ src/components/Post/Comments/Comments.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import { DiscussionEmbed } from "disqus-react";
 
 import { useSiteMetadata } from "@/hooks";
@@ -11,20 +10,29 @@ interface Props {
 
 const Comments: React.FC<Props> = ({ postTitle, postSlug }: Props) => {
   const { url, disqusShortname } = useSiteMetadata();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   if (!disqusShortname) {
     return null;
   }
 
   return (
-    <DiscussionEmbed
-      shortname={disqusShortname}
-      config={{
-        url: url + postSlug,
-        identifier: postTitle,
-        title: postTitle,
-      }}
-    />
+    <>
+      {isClient && (
+        <DiscussionEmbed
+          shortname={disqusShortname}
+          config={{
+            url: url + postSlug,
+            identifier: postTitle,
+            title: postTitle,
+          }}
+        />
+      )}
+    </>
   );
 };
 
