807383579ce7772e2ba98a36a2de690cbc4b12fd
nourabosen <nraabosen@gmail.com>
2025-10-31 14:57:02 +0200

    fix: ensure pages load at the top

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,2 +1,7 @@
 import "./src/assets/scss/main.scss";
 export { wrapRootElement } from "./internal/gatsby/wrap-root-element";
+
+export const shouldUpdateScroll = () => {
+  window.scrollTo(0, 0);
+  return false;
+};
