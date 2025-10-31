29ae79bc95784920a15e1839167a1cc63f9a8532
nourabosen <nraabosen@gmail.com>
2025-10-31 14:32:49 +0200

    fix: provide default props to Meta component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { useTheme } from "@/hooks";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { useSiteMetadata } from "@/hooks";
 import { Meta } from "@/components/Meta";
 
 import * as styles from "./Layout.module.scss";
@@ -13,6 +14,7 @@ interface Props {
 
 const Layout: React.FC<Props> = ({ children }: Props) => {
   const [{ mode }] = useTheme();
+  const { title, subtitle } = useSiteMetadata();
 
   useEffect(() => {
     document.documentElement.className = mode;
@@ -20,7 +22,7 @@ const Layout: React.FC<Props> = ({ children }: Props) => {
 
   return (
     <div className={styles.layout}>
-      <Meta />
+      <Meta title={title} description={subtitle} />
       <Header />
       <main className={styles.main}>{children}</main>
       <Footer />
