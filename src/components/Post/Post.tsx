999bb1945d6e157e7ef4658d063b23870e85adf4
nourabosen <nraabosen@gmail.com>
2025-10-31 16:58:28 +0200

    fix: implement final round of bug fixes

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -30,7 +30,6 @@ const Post: React.FC<Props> = ({ post }: Props) => {
             date={date}
             tags={tags || []}
             tagSlugs={tagSlugs || []}
-            tableOfContents={isClient && <TableOfContents html={html} isMobile />}
           />
         </div>
         <div className={styles.toc}>
