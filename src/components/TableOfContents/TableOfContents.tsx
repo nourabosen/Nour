0f72cc5448ea15d9239d061f3c638293c8462a47
nourabosen <nraabosen@gmail.com>
2025-10-31 15:36:28 +0200

    fix: implement final round of bug fixes

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -34,7 +34,16 @@ export const TableOfContents: React.FC<Props> = ({ html }) => {
     e.preventDefault();
     const element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
+      const offset = 100;
+      const bodyRect = document.body.getBoundingClientRect().top;
+      const elementRect = element.getBoundingClientRect().top;
+      const elementPosition = elementRect - bodyRect;
+      const offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
     }
     setIsOpen(false);
   };
